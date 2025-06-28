167ffc26e08a981f2a71be92d0d02c6b1dddf666
amine zarrouki <zarroukiamine2@gmail.com>
2025-06-28 15:22:39 +0200

    ouagfei

--- task_6.sql
+++ task_6.sql
@@ -1,12 +1,9 @@
--- Script to insert multiple customer records
--- Database name is passed as argument to mysql command
-
 INSERT INTO customers (
     customer_id,
     customer_name,
     email,
     address
 ) VALUES 
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
     (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
